8075853416e66aee8d3e83b7349030b0deef0085
vikaspsr <101392943+vikaspsr@users.noreply.github.com>
2022-12-18 12:30:47 +0530

    final project for MG_Nrega

---
+++ MG_Nrega/src/com/mgnrega/login/Main.java
@@ -0,0 +1,19 @@
+package com.mgnrega.login;
+
+//import com.mgnrega.*;
+
+public class Main {
+	public static void main(String[] args) {		
+		
+		// This is the main file
+		// Applications starts from here... 
+
+		
+		System.out.println("<==========  Welcome To MGNREGA  Project ================>");
+		
+		
+		UserActivity.selectUser();
+		
+	}
+
+}
